07fe3a5b1d20678fe3527afc2026d3b6b819e791
Ritik2712 <ritik27122mahajan@gmail.com>
2024-09-27 13:02:54 +0530

    updated education

--- utils/data/educations.js
+++ utils/data/educations.js
@@ -1,11 +1,5 @@
 export const educations = [
   {
-    id: 1,
-    title: "Masters of Computer Application",
-    duration: "2024 - Present",
-    institution: "Indira Gandhi National Open University",
-  },
-  {
     id: 2,
     title: "Bachelors of Computer Application",
     duration: "2020 - 2023",
