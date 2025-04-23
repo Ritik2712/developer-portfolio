07951390980d661e7b8f9df34101d68f17791184
Ritik2712 <ritik27122mahajan@gmail.com>
2025-04-23 23:28:17 +0530

    push:

--- app/api/contact/route.js
+++ app/api/contact/route.js
@@ -2,29 +2,29 @@ import axios from "axios";
 import { NextResponse } from "next/server";
 
 export async function POST(request) {
-  const payload = await request.json();
-  const token = process.env.TELEGRAM_BOT_TOKEN;
-  const chat_id = process.env.TELEGRAM_CHAT_ID;
+  // const payload = await request.json();
+  // const token = process.env.TELEGRAM_BOT_TOKEN;
+  // const chat_id = process.env.TELEGRAM_CHAT_ID;
 
-  if (!token || !chat_id) {
-    return NextResponse.json(
-      {
-        success: false,
-      },
-      { status: 200 }
-    );
-  }
+  // if (!token || !chat_id) {
+  //   return NextResponse.json(
+  //     {
+  //       success: false,
+  //     },
+  //     { status: 200 }
+  //   );
+  // }
 
   try {
-    const url = `https://api.telegram.org/bot${token}/sendMessage`;
-    const message = `New message from ${payload.name}\n\nEmail: ${payload.email}\n\nMessage:\n ${payload.message}\n\n`;
+    //   const url = `https://api.telegram.org/bot${token}/sendMessage`;
+    //   const message = `New message from ${payload.name}\n\nEmail: ${payload.email}\n\nMessage:\n ${payload.message}\n\n`;
 
-    const res = await axios.post(url, {
-      text: message,
-      chat_id: process.env.TELEGRAM_CHAT_ID,
-    });
+    //   const res = await axios.post(url, {
+    //     text: message,
+    //     chat_id: process.env.TELEGRAM_CHAT_ID,
+    //   });
 
-    if (res.data.ok) {
+    if (true) {
       return NextResponse.json(
         {
           success: true,
